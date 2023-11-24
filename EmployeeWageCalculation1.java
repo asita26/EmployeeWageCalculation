5dc0d241864f90eb0feabcb0f8aba7c43893446e
asita26 <asita98890@gmail.com>
2023-11-24 13:30:12 +0530

    Refactoring code and adding methods for each use case

--- EmployeeWageCalculation1.java
+++ EmployeeWageCalculation1.java
@@ -1,8 +1,8 @@
-
-import java.util.*;
+import java.util.Random;
 
 public class EmployeeWageCalculation1 {
-	private static final int FULL_TIME = 1;
+
+    private static final int FULL_TIME = 1;
     private static final int PART_TIME = 2;
     private static final int WAGE_PER_HOUR = 20;
     private static final int FULL_DAY_HOURS = 8;
@@ -10,46 +10,54 @@ public class EmployeeWageCalculation1 {
     private static final int WORKING_DAYS_IN_MONTH = 20;
     private static final int MAX_WORKING_HOURS = 100;
     private static final int MAX_WORKING_DAYS = 20;
-	static int checkingAttendance()
-    {
-    	  Random random = new Random();
-          return random.nextInt(3); // 0 for absent, 1 for full time, 2 for part time
+    
+    //Method for UC1 : Checking Attendance
+    public static int checkingAttendance() {
+        Random random = new Random();
+        return random.nextInt(3); // 0 for absent, 1 for full time, 2 for part time
+    }
+    
+    //Method for UC2 & UC3 : Calculating Wage for Employees
+    public static int calculateDailyWage(int empHours) {
+        return empHours * WAGE_PER_HOUR;
     }
 
-	public static void main(String[] args)
-	{
-        System.out.println("Welcome to Employee Wage Computation Program");
-        
-        int empHours=0;
-        int empWage=0;
+    //Method for UC5 & UC5 : Calculating Wage for Employees for a month and for total working hours
+    public static void calculateEmployeeWage() {
         int totalWorkingHours = 0;
         int totalWorkingDays = 0;
         int totalWage = 0;
 
-        while (totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours <= MAX_WORKING_HOURS) 
-        {
+        while (totalWorkingDays < MAX_WORKING_DAYS && totalWorkingHours <= MAX_WORKING_HOURS) {
             totalWorkingDays++;
-            int empCheck=checkingAttendance();
-          switch(empCheck)
-          {
-        
-            case FULL_TIME: empHours=FULL_DAY_HOURS;
-                            break;
-            case PART_TIME: empHours=PART_TIME_HOURS;
-                            break;
-            default: empHours=0;
-          
-          } 
-         
-         totalWorkingHours+= empHours;
-         System.out.println("Day: " +totalWorkingDays +" Attendance: " + empCheck +" EmployeeHours: "+empHours);
-       
-       }
-        
-        totalWage= totalWorkingHours * WAGE_PER_HOUR;
-       System.out.println("Total wage: $" +totalWage);
+            int empCheck = checkingAttendance();
+            int empHours;
+
+            switch (empCheck) {
+                case FULL_TIME:
+                    empHours = FULL_DAY_HOURS;
+                    break;
+                case PART_TIME:
+                    empHours = PART_TIME_HOURS;
+                    break;
+                default:
+                    empHours = 0;
+            }
+
+            totalWorkingHours += empHours;
+            int dailyWage = calculateDailyWage(empHours);
+            totalWage += dailyWage;
+
+            System.out.println("Daily Wage= " + dailyWage);
+        }
+
+        System.out.println("Total Wage= " + totalWage);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        calculateEmployeeWage();
     }
 }
-   
-		
+
 
