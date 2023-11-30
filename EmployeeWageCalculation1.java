f3988dce2df0b349a72620f6414dd21a8e4ac0c6
asita26 <asita98890@gmail.com>
2023-11-30 08:56:11 +0530

    Adding ability to save total wage for each company

--- EmployeeWageCalculation1.java
+++ EmployeeWageCalculation1.java
@@ -1,36 +1,48 @@
 import java.util.Random;
-
 public class EmployeeWageCalculation1 {
 
-    private static final int FULL_TIME = 1;
-    private static final int PART_TIME = 2;
-    private static final int WAGE_PER_HOUR = 20;
-    private static final int FULL_DAY_HOURS = 8;
-    private static final int PART_TIME_HOURS = 4;
-    private static final int WORKING_DAYS_IN_MONTH = 20;
-    private static final int MAX_WORKING_HOURS = 100;
-    private static final int MAX_WORKING_DAYS = 20;
-    
-    //Method for UC1 : Checking Attendance
-    public static int checkingAttendance() {
+    // Constants for employee attendance
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
+
+    // Instance variables for company-specific information
+    private String companyName;
+    private int wagePerHour;
+    private int maxWorkingDays;
+    private int maxWorkingHours;
+    private int totalWage;
+
+    // Constructor to initialize company-specific information
+    public EmpWageBuilder(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.maxWorkingDays = maxWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+        this.totalWage = 0;
+    }
+
+
+    public static int checkingAttendance()
+    {
         Random random = new Random();
         return random.nextInt(3); // 0 for absent, 1 for full time, 2 for part time
     }
-    
-    //Method for UC2 & UC3 : Calculating Wage for Employees
-    public static int calculateDailyWage(int empHours, int wagePerHour) {
-        return empHours * WAGE_PER_HOUR;
+
+
+    // Method for calculating daily wage
+    private int calculateDailyWage(int empHours) {
+        return empHours * wagePerHour;
     }
 
-    //Method for UC5 & UC5 : Calculating Wage for Employees for a month and for total working hours
-    public static void calculateEmployeeWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours)
-    {
+
+    // Method for calculating employee wage
+    public void calculateEmployeeWage() {
         int totalWorkingHours = 0;
         int totalWorkingDays = 0;
-        int totalWage = 0;
 
-        while (totalWorkingDays < maxWorkingDays  && totalWorkingHours <=  maxWorkingHours) 
-        {
+        while (totalWorkingDays < maxWorkingDays && totalWorkingHours <= maxWorkingHours) {
             totalWorkingDays++;
             int empCheck = checkingAttendance();
             int empHours;
@@ -47,22 +59,22 @@ public class EmployeeWageCalculation1 {
             }
 
             totalWorkingHours += empHours;
-            int dailyWage = calculateDailyWage(empHours, wagePerHour);
+            int dailyWage = calculateDailyWage(empHours);
             totalWage += dailyWage;
+
             System.out.println("Daily Wage for " + companyName + "= " + dailyWage);
-    
         }
 
-          System.out.println("Total Wage for " + companyName + "= " + totalWage);
-    
+        System.out.println("Total Wage for " + companyName + "= " + totalWage);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        // Example usage for two companies
-        calculateEmployeeWage("CompanyA", 20, 20, 100);
-        calculateEmployeeWage("CompanyB", 25, 25, 120);
+        EmpWageBuilder companyA = new EmpWageBuilder("CompanyA", 20, 20, 100);
+        companyA.calculateEmployeeWage();
+
+        EmpWageBuilder companyB = new EmpWageBuilder("CompanyB", 25, 25, 120);
+        companyB.calculateEmployeeWage();
     }
 }
-
 
