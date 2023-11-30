f23657bcd6917a34501bc26b85a31481b91ff57e
asita26 <asita98890@gmail.com>
2023-11-30 09:02:21 +0530

    Adding functionality to manage wage of multiple companies

--- EmployeeWageCalculation1.java
+++ EmployeeWageCalculation1.java
@@ -1,21 +1,13 @@
 import java.util.Random;
 public class EmployeeWageCalculation1 {
 
-    // Constants for employee attendance
-    public static final int FULL_TIME = 1;
-    public static final int PART_TIME = 2;
-    public static final int FULL_DAY_HOURS = 8;
-    public static final int PART_TIME_HOURS = 4;
-
-    // Instance variables for company-specific information
     private String companyName;
     private int wagePerHour;
     private int maxWorkingDays;
     private int maxWorkingHours;
     private int totalWage;
 
-    // Constructor to initialize company-specific information
-    public EmpWageBuilder(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+    public CompanyEmpWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
         this.companyName = companyName;
         this.wagePerHour = wagePerHour;
         this.maxWorkingDays = maxWorkingDays;
@@ -23,58 +15,120 @@ public class EmployeeWageCalculation1 {
         this.totalWage = 0;
     }
 
+    // Getters for company-specific information
+    public String getCompanyName() {
+        return companyName;
+    }
 
-    public static int checkingAttendance()
+    public int getWagePerHour() {
+        return wagePerHour;
+    }
+
+    public int getMaxWorkingDays() {
+        return maxWorkingDays;
+    }
+
+    public int getMaxWorkingHours() {
+        return maxWorkingHours;
+    }
+
+    // Setter for total wage
+    public void setTotalWage(int totalWage) {
+        this.totalWage = totalWage;
+    }
+
+    // Getter for total wage
+    public int getTotalWage() {
+        return totalWage;
+    }
+}
+
+public class EmpWageBuilder {
+
+    // Constants for employee attendance
+    public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
+    public static final int FULL_DAY_HOURS = 8;
+    public static final int PART_TIME_HOURS = 4;
+
+    // Array to store CompanyEmpWage objects
+    private CompanyEmpWage[] companyEmpWages;
+    private int numOfCompanies;
+
+    // Constructor to initialize array
+    public EmpWageBuilder() {
+        companyEmpWages = new CompanyEmpWage[5]; // Assuming a maximum of 5 companies
+        numOfCompanies = 0;
+    }
+
+    // Method to add a new company
+    public void addCompany(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
+        if (numOfCompanies < companyEmpWages.length) {
+            companyEmpWages[numOfCompanies] = new CompanyEmpWage(companyName, wagePerHour, maxWorkingDays, maxWorkingHours);
+            numOfCompanies++;
+        } else {
+            System.out.println("Cannot add more companies. Array full.");
+        }
+    }
+      // Method for checking attendance
+     public static int checkingAttendance()
     {
         Random random = new Random();
         return random.nextInt(3); // 0 for absent, 1 for full time, 2 for part time
     }
 
-
     // Method for calculating daily wage
-    private int calculateDailyWage(int empHours) {
+    public int calculateDailyWage(int empHours, int wagePerHour) {
         return empHours * wagePerHour;
     }
 
+  
 
-    // Method for calculating employee wage
+    // Method for calculating employee wage for all companies
     public void calculateEmployeeWage() {
-        int totalWorkingHours = 0;
-        int totalWorkingDays = 0;
+        for (int i = 0; i < numOfCompanies; i++) {
+            CompanyEmpWage currentCompany = companyEmpWages[i];
 
-        while (totalWorkingDays < maxWorkingDays && totalWorkingHours <= maxWorkingHours) {
-            totalWorkingDays++;
-            int empCheck = checkingAttendance();
-            int empHours;
+            int totalWorkingHours = 0;
+            int totalWorkingDays = 0;
 
-            switch (empCheck) {
-                case FULL_TIME:
-                    empHours = FULL_DAY_HOURS;
-                    break;
-                case PART_TIME:
-                    empHours = PART_TIME_HOURS;
-                    break;
-                default:
-                    empHours = 0;
+            while (totalWorkingDays < currentCompany.getMaxWorkingDays() && totalWorkingHours <= currentCompany.getMaxWorkingHours()) {
+                totalWorkingDays++;
+                int empCheck = checkingAttendance();
+                int empHours;
+
+                switch (empCheck) {
+                    case FULL_TIME:
+                        empHours = FULL_DAY_HOURS;
+                        break;
+                    case PART_TIME:
+                        empHours = PART_TIME_HOURS;
+                        break;
+                    default:
+                        empHours = 0;
+                }
+
+                totalWorkingHours += empHours;
+                int dailyWage = calculateDailyWage(empHours, currentCompany.getWagePerHour());
+                currentCompany.setTotalWage(currentCompany.getTotalWage() + dailyWage);
+
+                System.out.println("Daily Wage for " + currentCompany.getCompanyName() + "= " + dailyWage);
             }
 
-            totalWorkingHours += empHours;
-            int dailyWage = calculateDailyWage(empHours);
-            totalWage += dailyWage;
-
-            System.out.println("Daily Wage for " + companyName + "= " + dailyWage);
+            System.out.println("Total Wage for " + currentCompany.getCompanyName() + "= " + currentCompany.getTotalWage());
         }
-
-        System.out.println("Total Wage for " + companyName + "= " + totalWage);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        EmpWageBuilder companyA = new EmpWageBuilder("CompanyA", 20, 20, 100);
-        companyA.calculateEmployeeWage();
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
 
-        EmpWageBuilder companyB = new EmpWageBuilder("CompanyB", 25, 25, 120);
-        companyB.calculateEmployeeWage();
+        // Adding companies
+        empWageBuilder.addCompany("CompanyA", 20, 20, 100);
+        empWageBuilder.addCompany("CompanyB", 25, 25, 120);
+
+        // Calculate and display wages for all companies
+        empWageBuilder.calculateEmployeeWage();
     }
 }
 
